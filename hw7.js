6f6e0ab37c4afeeffb41ddbf2d312bf49a31fb79
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-12-16 18:39:32 +0200

    Add files via upload

---
+++ hw7.js
@@ -0,0 +1,48 @@
+// const number = [1, 2, 3];
+// number[1] = 10;  
+// console.log(num);
+
+
+// const string = ["1рядок", "2рядок", "3рядок"];
+// string.push("4рядок");  
+// console.log(string);
+
+
+// const num = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < num.length; i++) {
+//     sum += num[i];
+// }
+// console.log(sum);
+
+
+// const numbers = [174, 67, 798, 5475, 57];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+
+// const string = ["Данило", "Катя", "Анастасія", "Юля", "Маргарита"];
+// for (let k = 0; k < string.length; k++) {
+//     if (string[k].length > 5) { 
+//         console.log(string[k]);
+//     }
+// }
+
+
+// const numbers2 = [4, 5, 6, 28, 76, 1543, 47, 89, 73, 1];
+// let maximum = numbers2[0];
+// for (let l = 1; l < numbers2.length; l++) {   
+//     if (numbers2[l] > maximum) {
+//        maximum = numbers2[l];
+//     }
+//     console.log(maximum)
+// }
+
+
+// const lastPare = [12, 52, 55, 8, 53, 77, 17, 41, 4, 20];
+// for (let i = 0; i < lastPare.length; i++) {
+//     if (lastPare[i] % 2 === 0) {
+//         console.log(lastPare[i]);
+//     }
+// }
